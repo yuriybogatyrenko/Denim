b30400dc51ac998decac9c5dc5b257a422b219c2
geeekteam <info@geekteam.kz>
2017-05-15 19:04:38 +0600

    setup instruction state 1

--- app/js/app.js
+++ app/js/app.js
@@ -1,9 +1,33 @@
 'use strict';
 
 (function () {
-    $('.owl-carousel').owlCarousel({
+    $('.screenshots').owlCarousel({
         center: true,
         items: 2,
         startPosition: 1
     });
+
+    $('.alternative-slider').owlCarousel({
+        items: 3.5,
+        startPosition: 1
+    });
+
 })();
+
+
+
+$('.js-read-more-button').click(function () {
+
+    var readmore = $(this).prev(".read-more"),
+        readmoreHide = readmore.children(".read-more__wrapper"),
+        readmoreHeight = readmoreHide.height(),
+        $this = $(this);
+
+    if (readmore.hasClass("hidden")){
+        readmore.css('height', readmoreHeight);
+        setTimeout(function(){
+            readmore.removeClass("hidden");
+            $this.addClass("display-none");
+        }, 300)
+    }
+})
